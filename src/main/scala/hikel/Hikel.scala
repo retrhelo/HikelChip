ed36c2cf3577910657c69907c0b716dee6fbffda
retrhelo <artyomliu@foxmail.com>
2021-09-09 11:19:04 +0800

    add some tests

--- src/main/scala/hikel/Hikel.scala
+++ src/main/scala/hikel/Hikel.scala
@@ -3,7 +3,21 @@
 package hikel 
 
 import chisel3._
+import chisel3.util._
+
+import hikel.Config._
+import hikel.stage._
 
 class Hikel extends Module {
-	val io = IO(new Bundle {})
+	// genereate desired name for top-level module
+	override def desiredName: String = "SimTop"
+
+	val io = IO(new Bundle {
+		
+	})
+}
+
+import chisel3.stage.ChiselStage
+object HikelGenVerilog extends App {
+	(new ChiselStage).emitVerilog(new Hikel, BUILD_ARG)
 }
